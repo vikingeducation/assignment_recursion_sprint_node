3a280cf9634ecfdf618f8e0c977bcdd8ec39530d
Greg Filipczak <gregfilipczak@gmail.com>
2017-09-20 16:14:06 +0000

    digit sum COMPLETE

--- digitSum.js
+++ digitSum.js
@@ -12,21 +12,49 @@ Write an iterative method sumdigIterative which does the same iteratively.
 // 1, 3
 
 const sumdigRecursive = n => {
-  //base case, n is one digit
-  if (n <= 10) return n;
+  n = Math.abs(n);
+  if (n < 10) return n;
   const digit = Math.floor(n % 10);
   return digit + sumdigRecursive(Math.floor((n /= 10)));
 };
 
+console.log("recursive group");
 console.log(sumdigRecursive(1));
 console.log(sumdigRecursive(0));
 console.log(sumdigRecursive(10));
 console.log(sumdigRecursive(11));
 console.log(sumdigRecursive(101));
+console.log(sumdigRecursive(103));
+console.log(sumdigRecursive(1234));
+console.log(sumdigRecursive(-1234));
 
 const sumdigIterative = n => {
-  return n;
+  let result = 0;
+  while (n > 0) {
+    result += Math.floor(n % 10);
+    n = Math.floor(n / 10);
+  }
+  return result;
 };
-const digitAdd = () => {
-  return null;
+
+console.log("iterative group");
+console.log(sumdigIterative(1));
+console.log(sumdigIterative(0));
+console.log(sumdigIterative(10));
+console.log(sumdigIterative(11));
+console.log(sumdigIterative(101));
+console.log(sumdigIterative(103));
+console.log(sumdigIterative(1234));
+
+const digitAdd = n => {
+  return n
+    .toString()
+    .split("")
+    .map(n => parseInt(n))
+    .reduce((sum, el) => sum + el);
+  // return (result = n.map(n => parseInt(n)).reduce((sum, el) => sum + el));
 };
+
+console.log(digitAdd(101));
+console.log(digitAdd(103));
+console.log(digitAdd(1234));
