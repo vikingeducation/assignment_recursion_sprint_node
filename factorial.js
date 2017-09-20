ffb677a33b69fb52cb52d47262f1e2e93d9209d8
Alex-Willenbrink <awillenbrink@gmail.com>
2017-09-20 11:49:29 -0500

    working on palindrome madness

--- factorial.js
+++ factorial.js
@@ -4,25 +4,25 @@ const factorial = (n, acc) => (n <= 1 ? acc : factorial(n - 1, n * acc));
 const nonTCFactorial = n => (2 > n ? 1 : n * nonTCFactorial(n - 1));
 
 const facTimeStart = new Date().getTime();
-console.log('TC', getFactorial(5));
+console.log("TC", getFactorial(5));
 const facTimeEnd = new Date().getTime();
-console.log(facTimeEnd - facTimeStart, 'μs');
+console.log(facTimeEnd - facTimeStart, "μs");
 
 const facnonTCStart = new Date().getTime();
-console.log('non-TC', nonTCFactorial(5));
+console.log("non-TC", nonTCFactorial(5));
 const facnonTCEnd = new Date().getTime();
-console.log(facnonTCEnd - facTimeStart, 'μs');
+console.log(facnonTCEnd - facTimeStart, "μs");
 
 const factorialIter = n => {
-	n = 2 > n ? 1 : n;
-	let sum = n;
-	while (--n > 1) {
-		sum *= n;
-	}
-	return sum;
+  n = 2 > n ? 1 : n;
+  let sum = n;
+  while (--n > 1) {
+    sum *= n;
+  }
+  return sum;
 };
 
 const facIterStart = new Date().getTime();
-console.log('Iter', factorialIter(3));
+console.log("Iter", factorialIter(3));
 const facIterEnd = new Date().getTime();
-console.log(facIterEnd - facIterStart, 'μs');
+console.log(facIterEnd - facIterStart, "μs");
---
+++ palindrome.js
@@ -0,0 +1,23 @@
+const palRec = str => palindromeRecursive(str.trim().replace(/[^a-zA-Z]/g, ""));
+
+const palindromeRecursive = str =>
+  2 > str.length
+    ? true
+    : str[0] === str[str.length - 1] && palindromeRecursive(str.slice(1, -1));
+
+console.log("is Palindrome: ", palRec("race,,,,,,,,,car"));
+
+const palindromeIterative = str => {
+  str = str.trim().replace(/[^a-zA-Z]/g, "");
+  for (let i = 0, c = str.length - 1; i < c; i++, c--)
+    if (str[i] !== str[c]) return false;
+  return true;
+};
+
+const palindromeIterative = str => str.trim().replace(/[^a-zA-Z]/g, "").split("").reduce((acc, value, index) =>
+  str = ;
+  for (let i = 0, c = str.length - 1; i < c; i++, c--)
+    if (str[i] !== str[c]) return false;
+  return true;
+
+console.log("is palIter: ", palindromeIterative("ractycar"));
