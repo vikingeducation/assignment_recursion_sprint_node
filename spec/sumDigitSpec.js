7996d03554987c11cdfaaa19fb24af896240303c
Will Timpson <will.timpson@gmail.com>
2017-09-20 12:09:10 -0400

    iterative digit sum tests

--- spec/sumDigitSpec.js
+++ spec/sumDigitSpec.js
@@ -1,4 +1,4 @@
-const { sumDigitRecursive } = require("../sumDigit");
+const { sumDigitRecursive, sumDigitIterative } = require("../sumDigit");
 const sums = require("./sums");
 
 describe("The recursive digit sum function", () => {
@@ -11,5 +11,18 @@ describe("The recursive digit sum function", () => {
   it("recursively still works on really large numbers", () => {
     expect(sumDigitRecursive(22334)).toEqual(14);
     expect(sumDigitRecursive(8395134928)).toEqual(52);
+  });
+});
+
+describe("The iterative digit sum function", () => {
+  it("iteratively returns the correct sum of the digits of a given n", () => {
+    sums.forEach((sum, n) => {
+      expect(sumDigitIterative(n)).toEqual(sum);
+    });
+  });
+
+  it("iteratively still works on really large numbers", () => {
+    expect(sumDigitIterative(22334)).toEqual(14);
+    expect(sumDigitIterative(8395134928)).toEqual(52);
   });
 });
